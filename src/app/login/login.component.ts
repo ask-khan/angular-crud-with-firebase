5c0747530e02d0a5717691c2d0a77ac7a9864315
AhmedSaboorKhan <ahmed.saboor@huubia.com>
2020-11-10 11:22:57 +0500

    - Add bootstrap in new component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Login, SignUp } from './../signup-and-login';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  @Output()
-  toggleLoginAndSignUp = new EventEmitter<string>();
-  
-  constructor() { }
+	currentForm = new Login("","");
 
-  ngOnInit(): void { }
+	@Output()
+	toggleLoginAndSignUp = new EventEmitter<string>();
 
-  openSignUp() {
-    this.toggleLoginAndSignUp.emit();
-  }
+	constructor() { }
+
+	ngOnInit(): void { }
+
+	openSignUp() {
+		this.toggleLoginAndSignUp.emit();
+	}
+
+	onSubmit(formType) {
+		console.log('On submit' + formType);
+	}
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Login, SignUp } from './../signup-and-login';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+	selector: 'app-sign-up',
+	templateUrl: './sign-up.component.html',
+	styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
-  @Output()
-  toggleLoginAndSignUp = new EventEmitter<string>();
-  constructor() { }
+	currentForm = new SignUp("", "", "");
+	@Output()
+	toggleLoginAndSignUp = new EventEmitter<string>();
+	constructor() { }
 
-  ngOnInit(): void {
-    
-  }
-  
-  openLogin(){
-    this.toggleLoginAndSignUp.emit();
-  }
+	ngOnInit(): void {
+
+	}
+
+	openLogin() {
+		this.toggleLoginAndSignUp.emit();
+	}
+	
+	onSubmit(formType) {
+		console.log('On submit' + formType);
+	}
 
 }
